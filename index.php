e45b1620368e527a3fa1483799eb6d34de5f3d51
kenpeter <figo2476@hotmail.com>
2015-05-13 15:43:24 +1000

    youtube responsive layout

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>	
+		.video-wrapper {
+			width: 600px;
+			max-width: 100%;
+		}
+
+		.video-container {
+    	position: relative;
+    	padding-bottom: 56.25%;
+    	padding-top: 30px; height: 0; overflow: hidden;
+		}
+ 
+		.video-container iframe,
+		.video-container object,
+		.video-container embed {
+    	position: absolute;
+    	top: 0;
+    	left: 0;
+    	width: 100%;
+    	height: 100%;
+		}
+	</style>
+</head>
+<body>
+	<div class="video-wrapper">
+		<div class="video-container">
+			<iframe src="http://www.youtube.com/embed/dFVxGRekRSg" frameborder="0" width="560" height="315"></iframe>
+		</div>
+	</div>
+</body>
+</html>
